4f1ba9c7a06df07df813b9665ec7818e79462b5a
Pablo Araújo <pablo.oliveira@animaeducacao.com>
2021-01-20 15:37:34 -0300

    Criando tabela de dados.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container grid-lg">
+  <div class="container grid-lg my-2 py-2">
     <div class="card">
       <div class="card-header">
         <div class="h4">Todas as moedas</div>
       </div>
-      <div class="card-body"></div>
+      <div class="card-body">
+        <ListQuotes :quotes="quotes" />
+      </div>
     </div>
   </div>
 </template>
@@ -12,9 +14,11 @@
 <script>
 import { onMounted, reactive, toRefs } from "vue";
 import api from "@/services/api";
+import ListQuotes from "@/components/ListQuotes";
 
 export default {
   name: "App",
+  components: { ListQuotes },
   setup() {
     const data = reactive({
       quotes: {},
